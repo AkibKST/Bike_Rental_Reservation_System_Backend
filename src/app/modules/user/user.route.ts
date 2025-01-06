99cb6ffe93c073630485919764b364ce289b525b
AkibKST <akibkst22@gmail.com>
2025-01-06 12:02:16 +0600

    Create user controller route and service

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,9 @@
 import express from 'express'
+import { UserControllers } from './user.controller'
 
 const router = express.Router()
 
 //will call controller func
-router.post('/create-user', UserControllers.createStudent)
+router.post('/create-user', UserControllers.createUser)
 
 export const UserRoutes = router
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,1 +1,22 @@
-const createUser = {}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import config from '../../config'
+import { TUser } from './user.interface'
+
+// ekta user made korlam
+const createUserIntoDB = async (payload: TUser) => {
+  // create a user object
+  const userData: Partial<TUser> = {}
+
+  // if password is not given, use default password
+  userData.password = payload.password || (config.default_password as string)
+
+  // set user role
+  userData.role = 'user'
+
+  // set generated id
+}
+
+export const UserServices = {
+  createUserIntoDB,
+}
